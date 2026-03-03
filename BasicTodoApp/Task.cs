ff8a2122ab8a7f5bad65db4c90a4123ff9647c62
Jayden <Jayden@DESKTOP-50330FE>
2026-03-02 16:14:20 -0800

    Added the Task class

---
+++ BasicTodoApp/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicTodoApp
+{
+    public class TaskToDo
+    {
+        // what describes a task
+        public string Task { get; set; }
+        public string Due { get; set; }
+
+        // construct a new task
+        public TaskToDo()
+        {
+
+        }
+    }
+}
